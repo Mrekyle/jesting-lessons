390e5862fcab7317d75a8f250d790020e18b4401
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-07-01 13:43:02 +0100

    Add game object testing and game object

---
+++ js/scripts/simon.js
@@ -0,0 +1,10 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+}
+
+
+module.exports = { game } // Adding it in curly brackets allows us to export multiple at once in one line of code. So the code
+// doesn't need to be repeated over and over again 
--- js/test/simon.test.js
+++ js/test/simon.test.js
@@ -1,1 +1,31 @@
-const simonGame = require('../scripts/simon')
+const { game, currentGame } = require('../scripts/simon')
+// Same thing when importing the function into the testing file too. Using the {} allows multiple in one line of code
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('simon.html', ('utf-8'))
+    document.open();
+    document.write(fileContents)
+    document.close();
+})
+
+/**
+ * As this is the same testing block for the same variable we are able to perform all the tests inside the same code block.
+ */
+describe('Game object contains correct keys', () => {
+    test('score key exists', () => {
+        expect("score" in game).toBe(true)
+    })
+    test('currentGame key exists', () => {
+        expect("currentGame" in game).toBe(true) 
+    })
+    test('playerMoves key exists', () => {
+        expect("playerMoves" in game).toBe(true)
+    })
+    test('choices key exists', () => {
+        expect("choices" in game).toBe(true)
+    })
+    test('choices contains the correct ids', () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"])
+    })
+})
